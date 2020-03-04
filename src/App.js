a188413d8947e7efb74ee058b49ab8d05fdbc3d1
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-03-04 21:47:15 +0100

    feat: add bootstrap css

--- src/App.js
+++ src/App.js
@@ -4,22 +4,63 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="row  mt-5">
+      <div class="col-4 offset-2">
+        <h2> UserList</h2>
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action">Joe</a>
+
+          <a href="#" class="list-group-item list-group-item-action active">This is a primary list group item</a>
+          <a href="#" class="list-group-item list-group-item-action">This is a secondary list group item</a>
+          <a href="#" class="list-group-item list-group-item-action">This is a success list group item</a>
+        </div>
+        </div>
+    
+
+    <div class="col-4">
+      <h2>Joe</h2>
+      <hr />
+      <form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input type="text" class="form-control" id="name" />
+        </div>
+        
+        <div class="form-group">
+          <div class="mb-2">Droits</div>
+        
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="videoCheck" />
+              <label class="form-check-label" for="videoCheck">
+                vidéo
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="audioCheck" />
+              <label class="form-check-label" for="audioCheck">
+                audio
+              </label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="textCheck" />
+              <label class="form-check-label" for="textCheck">
+                text
+              </label>
+            </div>
+          </div>
+      
+        <hr />
+        <button class="btn btn-primary" type="submit">Save</button>
+
+      </form>
     </div>
+
+
+    </div>
+    </div>
+
+
   );
 }
 
